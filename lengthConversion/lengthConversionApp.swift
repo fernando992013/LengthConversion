45bac3022234579d414af9d56f26292330f5ed8f
Fernando Alvarez <fernando992013@gmail.com>
2024-05-05 18:29:29 -0400

    Initial Commit

---
+++ lengthConversion/lengthConversionApp.swift
@@ -0,0 +1,17 @@
+//
+//  lengthConversionApp.swift
+//  lengthConversion
+//
+//  Created by Fernando A on 5/5/24.
+//
+
+import SwiftUI
+
+@main
+struct lengthConversionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
